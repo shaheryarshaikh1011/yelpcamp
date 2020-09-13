b7f399efc1716c19db0b68f32d2b99a7c8f85264
Shaheryar Shaikh <43124039+avc52github@users.noreply.github.com>
2020-09-13 17:00:11 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,65 @@
+#yelpcamp
+
+Routes
+
+INDEX  /campgrounds
+NEW    /campgrounds/new
+CREATE /campgrounds
+SHOW   /campgrounds/:id
+
+NEW    /campgrounds/:id/comments/new  GET
+CREATE /campgrounds/:id/comments	  POST
+
+
+*add landing page
+*add campgrounds page
+
+each campground has
+*name
+*image
+
+#layout and styling
+
+*header footer
+*add in bootstrap
+
+
+#create new cg
+*setup cg route
+*add body-parser
+*setup form
+*add basic unstyled form
+
+
+#style
+*add better header
+*make cg display in grid
+
+#navbar and form
+*add navbar
+*style new cg form
+
+#show page
+*make show page
+
+#refactor mongoose code
+*use module.exports
+*requre everthing correctly
+
+#add seeds file
+*add seeds file
+*run seeds file everytime server starts
+
+#add comment model
+*make error go away
+*display comments
+
+#comment new/create
+*discuss nested routes
+*add comment new and create routes
+*add new comment form
+
+#style show page
+
+*add sidebar to show page
+*display comments nicely
---
+++ app.js
@@ -0,0 +1,150 @@
+var express = require("express");
+var app= express();
+var bodyParser=require("body-parser");
+var mongoose=require("mongoose");
+var Campground=require("./models/campground");
+var Comment =require("./models/comment");
+var seedDB=require("./seeds");
+
+app.use(express.static(__dirname + "/public"))
+seedDB();
+
+
+
+mongoose.connect("mongodb://localhost/yelp_camp");
+app.use(bodyParser.urlencoded({extended:true}));
+app.set("view engine","ejs");
+
+
+
+
+app.get("/",function(req,res) {
+	// body...
+	res.render("landing.ejs");
+})
+
+
+app.get("/campgrounds",function(req,res) {
+	
+	Campground.find({},function(err,allcampgrounds) {
+		if(err)
+		{
+			console.log(err);
+		}
+		else
+		{
+			res.render("campgrounds/index.ejs",{campgrounds:allcampgrounds});
+		}
+		// body...
+	})
+	
+	// body...
+})
+
+app.post("/campgrounds",function(req,res) {
+	var name=req.body.name;
+	var image=req.body.image;
+	var desc=req.body.description;
+	var newCampground={name:name,image:image,description:desc};
+	Campground.create(newCampground,function(err,newlyCreated) {
+		// body...if(err)
+		if(err)
+		{
+			console.log(err);
+		}
+		else
+		{	
+			res.redirect("/campgrounds");
+		}
+	})
+	
+	// body...
+})
+app.get("/campgrounds/new",function (req,res) {
+	// body...
+	res.render("campgrounds/new.ejs");
+})
+
+
+app.get("/campgrounds/:id",function(req,res) {
+	//find cg by id
+	Campground.findById(req.params.id).populate("comments").exec(function(err,foundCampground) {
+		// body...
+		if(err)
+		{
+			console.log(err);		
+		}
+		else
+		{
+
+			res.render("campgrounds/show",{campground:foundCampground});
+		}
+	})
+	
+	// body...
+});
+
+
+
+//=====================================================
+//comments routes
+//===============================================================
+
+app.get("/campgrounds/:id/comments/new",function(req,res) {
+	// body...
+	//find cg by id
+	Campground.findById(req.params.id,function(err,campground) {
+		// body...
+		if(err)
+		{
+			console.log(err);
+		}
+		else
+		{
+			res.render("comments/new",{campground:campground});
+		}
+	})
+	
+});
+
+
+app.post("/campgrounds/:id/comments",function(req,res) {
+	//lookup cg using id
+	Campground.findById(req.params.id,function(err,campground) {
+		if(err)
+		{
+			console.log(err);
+			res.redirect("/campgrounds");
+		}
+		else
+		{
+			//create new comment
+			Comment.create(req.body.comment,function(err,comment) {
+				if(err)
+				{
+					console.log(err);
+				}
+				else
+				{
+					campground.comments.push(comment);
+					campground.save();
+					//show page
+					res.redirect("/campgrounds/"+campground._id);
+				}
+				// body...
+			})
+		}
+		// body...
+	})
+	
+	//connect comment to cg
+	//redirect to show page
+
+})
+
+app.listen(3000,'localhost',function() {
+	// body...
+	console.log("Listening to port"+3000);
+	console.log("Yelpcamp Local Server has Started");
+
+});
